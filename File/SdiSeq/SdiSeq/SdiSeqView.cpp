e8b998e986d37a610e21f43a9e16b4f5b2f3c82c
Seo Hwi <chromatic_365@naver.com>
2022-11-02 23:30:54 +0900

    221102 MFC 공부 시작
      MFC로 시작을 했으나.. Win32 API 학습이 선행될 필요가 있기에 우선은 책위주로 공부를 하고, 추가 시간을 더 들여서 아삼개 블로그와 유튜브 강의로 기능적인 부분을 익히도록 할 계획.

---
+++ File/SdiSeq/SdiSeq/SdiSeqView.cpp
@@ -0,0 +1,128 @@
+﻿
+// SdiSeqView.cpp: CSdiSeqView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "SdiSeq.h"
+#endif
+
+#include "SdiSeqDoc.h"
+#include "SdiSeqView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CSdiSeqView
+
+IMPLEMENT_DYNCREATE(CSdiSeqView, CView)
+
+BEGIN_MESSAGE_MAP(CSdiSeqView, CView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CSdiSeqView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CSdiSeqView 생성/소멸
+
+CSdiSeqView::CSdiSeqView() noexcept
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CSdiSeqView::~CSdiSeqView()
+{
+}
+
+BOOL CSdiSeqView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CSdiSeqView 그리기
+
+void CSdiSeqView::OnDraw(CDC* /*pDC*/)
+{
+	CSdiSeqDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
+}
+
+
+// CSdiSeqView 인쇄
+
+
+void CSdiSeqView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CSdiSeqView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void CSdiSeqView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void CSdiSeqView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+void CSdiSeqView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CSdiSeqView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CSdiSeqView 진단
+
+#ifdef _DEBUG
+void CSdiSeqView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CSdiSeqView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CSdiSeqDoc* CSdiSeqView::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CSdiSeqDoc)));
+	return (CSdiSeqDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CSdiSeqView 메시지 처리기
---
+++ File/SdiSeq/SdiSeq/SdiSeqView.h
@@ -0,0 +1,52 @@
+﻿
+// SdiSeqView.h: CSdiSeqView 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CSdiSeqView : public CView
+{
+protected: // serialization에서만 만들어집니다.
+	CSdiSeqView() noexcept;
+	DECLARE_DYNCREATE(CSdiSeqView)
+
+// 특성입니다.
+public:
+	CSdiSeqDoc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual void OnDraw(CDC* pDC);  // 이 뷰를 그리기 위해 재정의되었습니다.
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 구현입니다.
+public:
+	virtual ~CSdiSeqView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // SdiSeqView.cpp의 디버그 버전
+inline CSdiSeqDoc* CSdiSeqView::GetDocument() const
+   { return reinterpret_cast<CSdiSeqDoc*>(m_pDocument); }
+#endif
+
