ead18f82ed09fa8674cca455b9de4f011af9a2eb
Chromatic-Hwi <chromatic_365@naver.com>
2022-11-05 12:10:24 +0900

    221105 리스트 박스 좌우 이동 실습 진행중
    - 질문1. VS 버전 문제인지 헤더 파일에 Dlg.h 파일이 생성되지 않았음.
    
    - 질문2. CMoveItemDlg와 CAboutDlg 차이
    
    - 내가 멍청했다. 프로젝트 생성할 때 헤더 이름이나 설정이 다르다 했더니 습관적으로 대화상자식이 아니라 단일문서로 만들고 있었음.
    
    - OnInitDialog()에 Cstring str 반복문을 추가해 항목을 추가하는 단계까지 진행.

---
+++ File/MoveItem/MoveItemDlg.h
@@ -0,0 +1,39 @@
+﻿
+// MoveItemDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMoveItemDlg 대화 상자
+class CMoveItemDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMoveItemDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MOVEITEM_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLbnSelchangeList2();
+	CListBox m_left_list;
+	afx_msg void OnLbnSelchangeRightList();
+	CListBox m_right_list;
+};
