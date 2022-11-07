1accb26b89520a502fb4e7a3037df6a3e091ec53
Seo Hwi <chromatic_365@naver.com>
2022-11-07 15:15:04 +0900

    221107 CImage 외부 이미지 파일 출력까지 구현(진행중)
    - 대화 상자에서 '확인' 버튼을 더블 클릭하면 OnBnClickedOk() 가 생성됨. 이 클래스 안에 CDialogEx::OnOK(); 를 주석 처리하고, OnBnClickedSendBtn();을 기입하면 '전송' 버튼에 해당하는 함수로 대체됨 (=다른 함수로 대체하면 엔터를 눌렀을 때 기능이 바뀐단 뜻.)
    
    - 이미지를 바꿔가면서 창에 출력해본 결과 사이즈가 달라도 화면이 바뀌거나 다른 사진 앞이나 뒤에 출력되진 않았음. 화면에 출력하는 job이 대체가 안되었기 때문이라고 '생각'함. 다른 이미지를 출력하려면 해당 job을 초기화하고 새로 선언해주는게 필요할 듯.

--- File/LoadPng/LoadPng/LoadPngDlg.cpp
+++ File/LoadPng/LoadPng/LoadPngDlg.cpp
@@ -65,6 +65,8 @@ BEGIN_MESSAGE_MAP(CLoadPngDlg, CDialogEx)
 	ON_WM_SYSCOMMAND()
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDOK, &CLoadPngDlg::OnBnClickedOk)
+	ON_BN_CLICKED(IDCANCEL, &CLoadPngDlg::OnBnClickedCancel)
 END_MESSAGE_MAP()
 
 
@@ -157,3 +159,17 @@ HCURSOR CLoadPngDlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
+
+
+void CLoadPngDlg::OnBnClickedOk()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	// CDialogEx::OnOK();
+}
+
+
+void CLoadPngDlg::OnBnClickedCancel()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	CDialogEx::OnCancel();
+}
