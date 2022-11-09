70a28e4f195d6913b2973975eacc0d01bbed0e4f
Seo Hwi <chromatic_365@naver.com>
2022-11-09 12:50:49 +0900

    221109 Picture Control 시도
    - StretchBlot로 변경 이후 가변 배율 휠 조작시 프로그램이 바로 꺼져버리는 문제 발생.
    
    - 이미지를 로드하는 m_image2가 널인지 검사해서 널인 경우, 단순히 배율 변화만 표시하고 이미지가 로드되었을 때만 가변 배율로 출력하도록 함.

--- File/ViewerTemp/ViewerTemp/ViewerTempDlg.cpp
+++ File/ViewerTemp/ViewerTemp/ViewerTempDlg.cpp
@@ -417,7 +417,7 @@ void CViewerTempDlg::OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar)
 	CDialogEx::OnHScroll(nSBCode, nPos, pScrollBar);
 }
 
-
+//
 void CViewerTempDlg::OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar)
 {
 	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
